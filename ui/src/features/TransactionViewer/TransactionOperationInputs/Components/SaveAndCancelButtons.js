8449d410d5ca7469cf4f9a8a874e041ef9b240d1
CarlGorse <carl.gorse@hotmail.co.uk>
2024-03-19 20:23:38 +0000

    Delete transaction

--- ui/src/features/TransactionViewer/TransactionOperationInputs/Components/SaveAndCancelButtons.js
+++ ui/src/features/TransactionViewer/TransactionOperationInputs/Components/SaveAndCancelButtons.js
@@ -1,11 +1,11 @@
 import { Button } from 'react-bootstrap';
 
-function SaveAndCancelButtons({ save, cancelTransactionOperation }) {
+function SaveAndCancelButtons({ save, cancelTransactionOperation, saveButtonText, cancelButtonText }) {
 
     return (
         <div style={{ marginTop: "20px" }}>
-            <Button size="sm" onClick={() => save()}>Save</Button>
-            <Button style={{ marginLeft: "1px" }} size="sm" onClick={() => cancelTransactionOperation()}>Cancel</Button>
+            <Button size="sm" onClick={() => save()}>{saveButtonText ?? "Save"}</Button>
+            <Button style={{ marginLeft: "1px" }} size="sm" onClick={() => cancelTransactionOperation()}>{cancelButtonText ?? "Cancel"}</Button>
         </div>
     );
 }
