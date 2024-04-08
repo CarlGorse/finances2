7fa53fb1a9e0eb759c6334efebb1d9fd2ca0bc82
CarlGorse <carl.gorse@hotmail.co.uk>
2024-04-08 09:03:34 +0100

    Add accordions

--- ui/src/common/recoil/atoms/SelectedBankAccountAtom.js
+++ ui/src/common/recoil/atoms/SelectedBankAccountAtom.js
@@ -3,6 +3,7 @@ import { atom } from 'recoil';
 export const selectedBankAccountAtom = atom({
     key: "selectedBankAccountState",
     default: {
-        AccountId: 7
+        AccountId: 7,
+        Name: "Natwest"
     }
 });
--- ui/src/features/TransactionViewer/TransactionViewer.js
+++ ui/src/features/TransactionViewer/TransactionViewer.js
@@ -1,18 +1,24 @@
+import Accordion from 'react-bootstrap/Accordion';
 import BankAccountSelector from './BankAccountSelector';
-import { Container } from 'react-bootstrap';
 import OperationButtons from './OperationButtons';
 import OperationPage from './OperationPage';
+import { padStart } from 'common/functions/NumberFunctions';
+import { selectedBankAccountAtom } from 'common/recoil/atoms/SelectedBankAccountAtom';
 import TransactionList from './TransactionList';
 import { transactionOperationAtom } from 'common/recoil/atoms/TransactionOperationAtom';
 import { useRecoilState } from 'recoil';
 import UserMessage from 'common/components/UserMessage'
 import { useEffect, useState } from 'react'
+import { useRecoilValue } from 'recoil'
 import YearsAndPeriodsSearch from './YearsAndPeriodsSearch';
+import { yearAndPeriodSearchAtom } from 'common/recoil/atoms/YearAndPeriodSearchAtom';
 
 function TransactionViewer() {
 
+  const selectedBankAccount = useRecoilValue(selectedBankAccountAtom);
   const [transactionOperation, setTransactionOperation] = useRecoilState(transactionOperationAtom);
   const [show, setShow] = useState(false);
+  const yearAndPeriodSearch = useRecoilValue(yearAndPeriodSearchAtom);
 
   useEffect(() => {
     if (transactionOperation) {
@@ -22,16 +28,10 @@ function TransactionViewer() {
 
   return (
 
-    <Container>
+    <Accordion>
 
-      <BankAccountSelector />
-
-      <div style={{ marginTop: "20px" }}>
-        <YearsAndPeriodsSearch />
-      </div>
-
-      <div style={{ marginTop: "30px" }}>
-        <OperationButtons />
+      <div style={{ marginLeft: "20px" }}>
+        <BankAccountSelector />
       </div>
 
       <div style={{ marginTop: "20px" }}>
@@ -39,14 +39,40 @@ function TransactionViewer() {
       </div>
 
       <div style={{ marginTop: "20px" }}>
-        <OperationPage show={show} handleClose={() => { setShow(false); setTransactionOperation(null); }} />
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            Search transactions: <b>
+              {padStart(yearAndPeriodSearch.StartPeriod, 2, '0')}.{yearAndPeriodSearch.StartYear}
+            </b>
+            {" to "}
+            <b>
+              {padStart(yearAndPeriodSearch.EndPeriod, 2, '0')}.{yearAndPeriodSearch.EndYear}
+            </b>
+          </Accordion.Header>
+          <Accordion.Body>
+            <YearsAndPeriodsSearch />
+          </Accordion.Body>
+        </Accordion.Item>
       </div>
+
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>
+          Operations
+        </Accordion.Header>
+        <Accordion.Body>
+
+          <OperationButtons />
+
+          <OperationPage show={show} handleClose={() => { setShow(false); setTransactionOperation(null); }} />
+
+        </Accordion.Body>
+      </Accordion.Item>
 
       <div style={{ marginTop: "20px" }}>
         <TransactionList />
       </div>
 
-    </Container >
+    </Accordion >
   );
 }
 
